99a4ef1e92cbe154263e484d7a4ddd4eb7c472f9
Luca Cambiaghi <luca.cambiaghi@me.com>
2021-12-24 12:31:45 +0100

    Updated flake.nix for M1

---
+++ darwin/bootstrap/default.nix
@@ -0,0 +1,60 @@
+{ config, pkgs, lib, ... }:
+
+{
+  # Nix configuration ------------------------------------------------------------------------------
+
+  nix.binaryCaches = [
+    "https://cache.nixos.org/"
+  ];
+  nix.binaryCachePublicKeys = [
+    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+  ];
+  nix.trustedUsers = [
+    "@admin"
+  ];
+  users.nix.configureBuildUsers = true;
+
+  # Enable experimental nix command and flakes
+  # nix.package = pkgs.nixUnstable;
+  nix.extraOptions = ''
+    auto-optimise-store = true
+    experimental-features = nix-command flakes
+  '' + lib.optionalString (pkgs.system == "aarch64-darwin") ''
+    extra-platforms = x86_64-darwin aarch64-darwin
+  '';
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+
+
+  # Shells -----------------------------------------------------------------------------------------
+
+  # Add shells installed by nix to /etc/shells file
+  environment.shells = with pkgs; [
+    bashInteractive
+    fish
+    zsh
+  ];
+
+  # Make Fish the default shell
+  programs.fish.enable = true;
+  programs.fish.useBabelfish = true;
+  programs.fish.babelfishPackage = pkgs.babelfish;
+  # Needed to address bug where $PATH is not properly set for fish:
+  # https://github.com/LnL7/nix-darwin/issues/122
+  programs.fish.shellInit = ''
+    for p in (string split : ${config.environment.systemPath})
+      if not contains $p $fish_user_paths
+        set -g fish_user_paths $fish_user_paths $p
+      end
+    end
+  '';
+  environment.variables.SHELL = "${pkgs.fish}/bin/fish";
+
+  # Install and setup ZSH to work with nix(-darwin) as well
+  programs.zsh.enable = true;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+}
